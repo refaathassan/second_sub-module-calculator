49b3173a9d9d674e6c1d0811954465294ff18f71
Refaat Hassan <refathassan1289641@gmail.com>
2024-12-25 01:35:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+first_num=float(input("please enter the first number:   "))
+second_num=float(input("please enter the second number:   "))
+sumation=first_num+second_num
+subtraction=first_num-second_num
+multiplication=first_num*second_num
+division=0
+if second_num< first_num :
+    division=first_num/second_num
+print("the sumation  ",sumation)
+print("the subtraction  ",subtraction)
+print("the multiplication  ",multiplication)
+print("the division  ",division)
